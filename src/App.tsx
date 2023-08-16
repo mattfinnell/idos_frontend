00f3a4b17386e0d0c6f12af8cb3b0d97ef0c49f6
Matt Finnell <mattfinnell104@gmail.com>
2023-08-16 19:53:13 +0000

    Added ChakraUI and Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
+import { ChakraProvider } from '@chakra-ui/react';
+import NavBarWithSubnavigation from './components/NavBar/NavBar';
 
 const fetchTodo = () => fetch("http://localhost:9000/todo").then(response => response.json());
 
@@ -9,23 +11,26 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <HealthCheck />
-        </header>
-      </div>
+      <ChakraProvider>
+        <div className="App">
+          <NavBarWithSubnavigation/>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            <HealthCheck />
+          </header>
+        </div>
+      </ChakraProvider>
     </QueryClientProvider>
   );
 }
