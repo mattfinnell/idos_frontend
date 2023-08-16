e175a1b0505f67ab145b2578fa98470e8ded420c
Matt Finnell <mattfinnell104@gmail.com>
2023-08-16 21:46:11 +0000

    updated development environment editor settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ChakraProvider } from '@chakra-ui/react';
-import NavBarWithSubnavigation from './components/Navigation/NavBar';
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { routes } from './components/Navigation/routes';
+import NavBarWithSubnavigation from "./components/Navigation/NavBar";
+import { routes } from "./components/Navigation/routes";
 
 const queryClient = new QueryClient();
 
@@ -14,10 +14,12 @@ function App() {
       <ChakraProvider>
         <BrowserRouter>
           <div className="App">
-            <NavBarWithSubnavigation/>
-              <Routes>
-                {routes.map((route) => <Route path={route.path} Component={route.component}/>)}
-              </Routes>
+            <NavBarWithSubnavigation />
+            <Routes>
+              {routes.map((route) => (
+                <Route path={route.path} Component={route.component} />
+              ))}
+            </Routes>
           </div>
         </BrowserRouter>
       </ChakraProvider>
